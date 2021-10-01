2ad92cdd8919cc95f3e4c5488d4e7fc54d82facb
ArisGoi <aris.goi97@gmail.com>
2021-10-01 17:18:39 +0200

    add albums

--- src/components/Album.vue
+++ src/components/Album.vue
@@ -1,11 +1,19 @@
 <template>
-  <div></div>
+  <div class="album">
+      <img :src="alb.poster" :alt="`${alb} Cover`">
+      <h2>{{alb.title}}</h2>
+      <p class="autore">{{alb.author}}</p>
+      <p class="anno">{{alb.year}}</p>
+  </div>
 </template>
 
 <!-- SCRIPTS -->
 <script>
 export default {
   name: 'Album',
+  props: {
+      alb: Object,
+  }
 }
 </script>
 
@@ -13,4 +21,27 @@ export default {
 <style scoped lang="scss">
 @import '../assets/style/style.scss';
 
+    .album{
+        background-color: $bg-header;
+        padding: 15px;
+        text-align: center;
+        height: 100%;
+        overflow: hidden;
+
+        img{
+            width: 100%;
+        }
+
+        h2{
+            margin: 20px 0;
+            text-transform: uppercase;
+            color: white;
+            font-size: 26px;
+        }
+        p{
+            margin: 2px 0;
+            color: lightslategray;
+            font-size: 20px;
+        }
+    }
 </style>
--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="container-m">
-      <Album/>
+      <div class="albCont" v-for="(album, index) in albumsData" :key="index">
+          <Album :alb="album"/>
+      </div>
   </div>
 </template>
 
 <!-- SCRIPTS -->
 <script>
 import Album from './Album.vue'
+import axios from 'axios';
 
 export default {
   name: 'Albums',
   components: {
       Album
+  },
+  data(){
+      return{
+          albumsData: [],
+      }
+  },
+  created(){
+      axios
+      .get('https://flynn.boolean.careers/exercises/api/array/music')
+      .then((response) => {
+          this.albumsData = response.data.response;
+      });
   }
 }
 </script>
@@ -19,5 +34,16 @@ export default {
 <!-- SCOPED STYLE -->
 <style scoped lang="scss">
 @import '../assets/style/style.scss';
+
+.container-m{
+    display: flex;
+    flex-wrap: wrap;
+    padding-top: 40px;
+
+    .albCont{
+        width: calc(100% / 5);
+        padding: 10px;
+    }
+}
 
 </style>
